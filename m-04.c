e9644a924768ded5c6cca2f71cefd1bfefccce4c
Andrew-NiNo <muravey1@aport.ru>
2021-03-16 20:56:52 +0300

    refactoring

--- m-04.c
+++ m-04.c
@@ -1,62 +1,51 @@
-// Задача M-04 Задано два массива натуральных чисел
-// Тот массив среднее арифметическое которого большее - вывести по убыванию, а тот у которого среднее арифметическое меньше - вывести по увеличению.
+// The task M-04
+// Two arrays of natural sizes are given
+// The array whose arithmetic mean is greater - output in descending order, and the one with less arithmetic mean - display in increasing order.
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 500
 
- int my_mean (int *s, int count) {				//Функция my-mean возвращает среднее арифметическое значение массива
-	 
-	int j = 0;
- 
-	int medium = 0;
-	 
+int mean (int *array_int, size_t size) {				// The "mean" function returns the arithmetic mean of the array
+
 	int sum = 0;
-	 
-	for (int i = 0; i < count; i++) { 								
-		
-		 sum += s[j++];
-		
-	}
-		medium = sum / count;
-		
- return medium;	
- 
- }	
 
- void my_sorting(int *digit, char c, int number) {   // Функция сортировки массива принимает три значения: массив, 
-													  //  знак '<' или '>' и количество элементов массива.
-	int i;
-	int j;
-	
-	if (c == '<') {								//Если знак "меньше" ('<') 
-		 
-		for (i = number - 1; i > 0; i--) {
-		
-			for (j = 0; j < i; j++) {
-								
-			    if (digit[j] < digit[j + 1]){ 		//Происходит сортировка массива по уменьшению значений
-				
-					int temp = digit[j];
-					digit[j] = digit[j + 1];
-					digit[j + 1] = temp;
+	for (size_t i = 0; i < size; i++) {
+
+		sum += array_int[i++];
+	}
+
+	return sum / size;
+}
+
+void ReplacementOfValues (int *array_int, size_t index) {
+
+	int temp = array_int[index];
+	array_int[index] = array_int[index + 1];
+	array_int[index + 1] = temp;
+}
+
+void my_sorting(int *digits, char control, size_t size) {   	
+
+	size_t i;
+	size_t j;
+
+	for (i = size - 1; i > 0; i--) {
+
+		for (j = 0; j < i; j++) {
+
+			if (control == '<') {
+
+				if (digits[j] < digits[j + 1]) { 		// Sort the array by decreasing values
+
+					ReplacementOfValues(digits, j);
 				}
 			}
-		}
-	}
-	
-	else if (c == '>') {							//Если знак "больше" ('>') 
-		
-		for (i = number - 1; i > 0; i--) {
-		
-			for (j = 0; j < i; j++) {
-									
-			    if(digit[j] > digit[j + 1]){ 		//Происходит сортировка массива по увеличению значений
-				
-					int temp = digit[j];
-					digit[j] = digit[j + 1];
-					digit[j + 1] = temp;
+			else if (control == '>') {							
+
+				if (digits[j] > digits[j + 1]) { 		// Sort the array by increasing values
+
+					ReplacementOfValues(digits, j);
 				}
 			}
 		}
@@ -64,40 +53,37 @@
 }
 
 
- int main(void) {
+int main(void) {
 
-	int array_1[13] = {34,5,67,245,776,34,22,98,13,34,68,231};
-	int array_2[10] = {86,564,343,577,2,34,4,7,2};
-	int i = 0;
-	int s_1 = 0;
-	int s_2 = 0;
-	
-		
-		s_1 = my_mean(array_1, 12);							//Считаем среднее арифметическое значение массивов
-	
-		s_2 = my_mean(array_2, 9);	
-	
-		
-		printf("\n mean array_1 %d  array_2 %d\n\n", s_1, s_2);
-			
-	
-	my_sorting(array_1, s_1 > s_2 ? '<' : '>', 12);		//Запускаем функцию сортировки 
-	
-   	for ( i = 0; i < 12; i++ ) {										//Выводим массив на экран
-					  
-    printf(" %d ", array_1[i]);
-	
+	int array_1[] = {34, 5, 67, 245, 776, 34, 22, 98, 13, 34, 68, 231};
+	int array_2[] = {86, 564, 343, 577, 2, 34, 4, 7, 2};
+
+	size_t size_arr_1 = sizeof(array_1) / sizeof(array_1[0]);
+	size_t size_arr_2 = sizeof(array_2) / sizeof(array_2[0]);
+
+
+	int mean_array_1 = mean(array_1, size_arr_1);							
+
+	int mean_array_2 = mean(array_2, size_arr_2);
+
+	printf("\n  mean \tarray_1 = %d\n  \t\tarray_2 = %d\n\n", mean_array_1, mean_array_2);
+
+
+	my_sorting(array_1, mean_array_1 > mean_array_2 ? '<' : '>', size_arr_1);					// Sorting		
+
+	for ( size_t i = 0; i < size_arr_1; i++ ) {										
+
+		printf(" %d ", array_1[i]);
 	}
-	
+
 	printf("\n\n");
-		
-	my_sorting(array_2, s_1 > s_2 ? '>' : '<', 9);			//Запускаем функцию обратной сортировки 
-	
-	for ( i = 0; i < 9; i++ ) {											//Выводим массив на экран
-					  
-    printf(" %d ", array_2[i]);
-	
+
+	my_sorting(array_2, mean_array_1 > mean_array_2 ? '>' : '<', size_arr_2);					// Revers sorting	
+
+	for ( size_t i = 0; i < size_arr_2; i++ ) {										
+
+		printf(" %d ", array_2[i]);
 	}
- 
- return 0;
+
+	return 0;
 }
