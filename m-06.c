94a3a0bd015bebfc244de6424fc41862a8bf8f0e
Andrew-NiNo <muravey1@aport.ru>
2021-03-24 22:06:25 +0300

    refactoring

--- m-06.c
+++ m-06.c
@@ -8,35 +8,29 @@
 
 #define MAX 500
 
- int main(void) {
-
-	char str[MAX] = {0};
-	char *ptr = str;
+int main(void)
+{
+	char *ptr = malloc(255);
 	int max = INT_MIN;
 	int min = INT_MAX;
 
-	printf("\n Enter value : \n");					
-	 	 
-	fgets(str, MAX, stdin);
+	printf("\n Enter value : \n");
 
-	while (*ptr) { 									
-	
-		if (isdigit(*ptr)) {
-      
-		 int value = strtol(ptr, &ptr, 10); 
-	 
-			if (max < value) {						
-		   
-			   max = value;
-			}
-			else if (min > value) {					
-		   
-					 min = value;
-				}
-		} 
-		else  ptr++;
+	fgets(ptr, MAX, stdin);
+
+	while (*ptr)
+	{
+		if (isdigit(*ptr))
+		{
+			int value = strtol(ptr, &ptr, 10);
+
+			max < value? max = value: 0;
+			min > value? min = value: 0;
+		}
+		else
+			ptr++;
 	}
-	printf("\n Summ min & max = %d\n", max + min); 
- 
- return 0;
+	printf("\n Summ min & max = %d\n", max + min);
+
+	return 0;
 }
